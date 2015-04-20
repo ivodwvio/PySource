4f121e4ff48e901cc937069f16a41df6a345a81f
samuraix <ivodwvio@mail.bg>
2015-04-20 21:13:52 +0300

    Removing temp files

--- task1.py
+++
@@ -1,5 +0,0 @@
-# Show the numbers from 0 to 9
-
-for i in range(10):
-	print(i, end=' ')
-print()
--- task2.py
+++
@@ -1,6 +0,0 @@
-# The numbers from 0 to 100
-
-for i in range(101):
-	if (i % 10 == 0 and i != 0):
-		print()
-	print(i, end=' ')
