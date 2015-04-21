15d445b18c51512264bc572f4f6c5f09a1c3abb9
samuraix <ivodwvio@mail.bg>
2015-04-21 20:45:31 +0300

    Add some lambda exercises.

--- tutorial.py
+++ tutorial.py
@@ -284,3 +284,49 @@ parrot(voltage = 1000000, action = 'VOOOOOM')
 parrot(action = 'VOOOOOM', voltage = 1000000)
 parrot('a million', 'bereft of life', 'jump')
 parrot('a thousand', state = 'pushing up the daisies')
+
+def cheeseshop(kind, *arguments, **keywords):
+	print('-- Do you have any', kind, '?')
+	print('-- I\'m sorry, we\'re all out of', kind)
+	for arg in arguments:
+		print(arg)
+	print('-' * 40)
+	keys = sorted(keywords.keys())
+	for kw in keys:
+		print(kw, ':', keywords[kw])
+
+cheeseshop('Limburger', 'It\'s very runny, sir.',
+		   'It\'s really very, VERY runny, sir.',
+		   shopkeeper = 'Michael Palin',
+		   client = 'John Cleese',
+		   sketch = 'Cheese Shop Sketch')
+
+def write_multiple_items(file, separator, *args):
+	file.write(separator.join(args))
+
+def concat(*args, sep = '/'):
+	return sep.join(args)
+
+print(concat('earth', 'mars', 'venus'))
+print(concat('earth', 'mars', 'venus', sep = '.'))
+
+print(list(range(3, 6)))
+args = [3, 6]
+print(args)
+print(list(range(*args)))
+
+def parrot(voltage, state = 'a stiff', action = 'voom'):
+	print('-- This parrot wouldn\'t', action, end = ' ')
+	print('if you put', voltage, 'volts through it.', end=' ')
+	print('E\'s', state, '!')
+
+d = {'voltage': 'four million', 'state': 'bleedin\' demised', 'action': 'voom'}
+parrot(**d)
+
+def make_incrementor(n):
+	return lambda x: x + n
+
+f = make_incrementor(42)
+
+f(0)
+f(1)
